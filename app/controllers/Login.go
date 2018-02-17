98862dba7cb516ce917fb07a212ba0f16b221dc7
GitSUmito <tsukada@sumito.jp>
2018-02-17 15:00:04 +0900

    modified

--- app/controllers/Login.go
+++ app/controllers/Login.go
@@ -1,12 +1,12 @@
 package controllers
 
 import (
-    "github.com/revel/revel"
-    _ "github.com/go-sql-driver/mysql"
-    "github.com/jmoiron/sqlx"
-    "log"
-    "fmt"
-//    "github.com/myaccount/myapp/app/model"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/revel/revel"
+	"log"
+	//    "github.com/myaccount/myapp/app/model"
 )
 
 type Login struct {
@@ -14,39 +14,52 @@ type Login struct {
 }
 
 type User struct {
-    Id   int    `db:"id"`
-    Name string `db:"name"`
+	Id   int    `db:"id"`
+	Name string `db:"name"`
 }
 
-func (c Login) Index(Id string, Password int) revel.Result {
+func (c Login) Index(Id int, Password int) revel.Result {
 
-        fmt.Printf("%#v\n%#v\n", Id, Password)
-        d := UserLogin(Id, Password) 
-        
+	fmt.Printf("%#v\n%#v\n", Id, Password)
+
+	c.Validation.Required(Id).Message("IDを入力してください")
+	// c.Validation.MinSize(Id,3).Message("IDが正しいか確認してください")
+
+	c.Validation.Required(Password).Message( "電話番号を入力してください")
+	// c.Validation.MinSize(Password, 3).Message( "電話番号が正しいか確認してください")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
+	d := UserLogin(Id, Password)
+
 	return c.Render(d)
 }
 
+func UserLogin(Id int, Password int) []User {
 
-func UserLogin(Id string, Password int) []User {
+	db, err := connect()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    db, err := connect()
-    if err != nil {
-        log.Fatalln(err)
-    }
+	people := []User{}
+	err = db.Select(&people, "SELECT id, name From patient where id = ?;", Id)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    people := []User{}
-    err = db.Select(&people, "SELECT id, name From patient where id = ?;", Id)
-    if err != nil {
-        fmt.Println(err)
-    }
+	for i := 0; i < len(people); i++ {
+		fmt.Println(people[i], " ", people[i].Id, " ", people[i].Name, " ")
+	}
 
-    for i :=0; i <len(people); i++ {
-        fmt.Println(people[i], " ", people[i].Id, " ", people[i].Name, " ")
-    }
-
-    return people
+	return people
 }
 
 func connect() (*sqlx.DB, error) {
-    return sqlx.Connect("mysql", "root:password@(localhost:3306)/booking")
+	return sqlx.Connect("mysql", "root:password@(localhost:3306)/booking")
 }
+
